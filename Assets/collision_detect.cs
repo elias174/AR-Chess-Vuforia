64163f9eec4d4cc5b1959cfb002a4c0572bbc9ea
elias174 <elias.valencia@ucsp.edu.pe>
2019-09-25 21:24:12 -0500

    script collide

--- Assets/collision_detect.cs
+++ Assets/collision_detect.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class collision_detect : MonoBehaviour
 {
+    float timer = 0;
+    float timer_position = 0;
     public GameObject bishop;
+    public GameObject image_target_piece;
+    public GameObject board_image_targe;
     public int redcol;
     public int greencol;
     public int bluecol;
+    public float y_piece;
+    public string name_object;
     public bool is_selected = false;
+    public bool position_target_selected = false;
+    float timerSec = 0;
     public Color32 original_color;
     // Start is called before the first frame update
     void Start()
     {
-        bishop = GameObject.Find("Bishop");
+        bishop = GameObject.Find(name_object);
+        image_target_piece = GameObject.Find("piece");
+        board_image_targe = GameObject.Find("ImageTarget");
         original_color = bishop.GetComponent<Renderer>().material.GetColor("_Color");
     }
 
@@ -23,6 +33,26 @@ public class collision_detect : MonoBehaviour
         if (is_selected)
         {
             bishop.GetComponent<Renderer>().material.color = new Color32((byte)redcol, (byte)greencol, (byte)bluecol, 255);
+            if(timer > 4)
+            {
+                bishop.transform.SetParent(image_target_piece.transform);
+                is_selected = false;
+            }
+            timer += Time.deltaTime;
+            // Vector3 pos = bishop.transform.position;
+            // pos.y = y_piece;
+            // bishop.transform.position = pos;
+        }
+        else if (position_target_selected)
+        {
+            if (timer_position > 4)
+            {
+                Debug.Log("changing... finish");
+                bishop.transform.SetParent(board_image_targe.transform);
+                position_target_selected = false;
+            }
+            Debug.Log(timer_position);
+            timer_position += Time.deltaTime;
         }
     }
 
@@ -30,23 +60,38 @@ public class collision_detect : MonoBehaviour
     {
         if (collision.gameObject.name == "cube")
         {
-            Debug.Log("Collision detected");
+            // Debug.Log("Collision detected");
             is_selected = true;
+        }
+        else if (collision.gameObject.name == "position")
+        {
+            position_target_selected = true;
+            Debug.Log("Collide in target position");
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        Debug.Log("Finished collision");
-        is_selected = false;
-        bishop.GetComponent<Renderer>().material.color = original_color;
+        // Debug.Log("Finished collision");
+        if (collision.gameObject.name == "cube")
+        {
+            is_selected = false;
+            bishop.GetComponent<Renderer>().material.color = original_color;
+            timer = 0;
+        }
+        else if (collision.gameObject.name == "position")
+        {
+            position_target_selected = false;
+            Debug.Log("Exit collision");
+            timer_position = 0;
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "cube")
         {
-            Debug.Log("Trigger eventtt");
+            //Debug.Log("Trigger eventtt");
         }
     }
 }
